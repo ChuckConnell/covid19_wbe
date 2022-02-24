01acbfbe526202cad3d5bd07ebd32ed1f65a2870
Chuck Connell <chuck.connell.3@gmail.com>
2022-02-24 18:12:53 -0500

    Do raw and analytics. Create anonymized samples.

--- Make Enhanced USA WBE Sample File.py
+++ Make Enhanced USA WBE Sample File.py
@@ -1,5 +1,5 @@
 
-# Take the full CDC NWSS dataset and add fields for vax status, cases, hospitalizations and deaths,
+# Take the full CDC NWSS datasets and add fields for vax status, cases, hospitalizations and deaths,
 # in that location at that time. But be smart about what dates we use for vax, death, etc since these
 # dates need a setback or set-forward to be meaningful to the wastewater signal.
 
@@ -9,13 +9,20 @@ VAX_LOOK_BACK = 10     # how far back from sample date do we look for vax info
 CASES_LOOK_AHEAD = 7     # how far ahead of sample do we look for case info
 HOSP_LOOK_AHEAD = 14     # how far ahead of sample do we look for hospitalization info
 DEATHS_LOOK_AHEAD = 21     # how far ahead of sample do we look for mortality info
-SAMPLES_OUTPUT_FILE = "NwssRawEnhanced.tsv"
+RAW_OUTPUT_FILE = "NwssRawEnhanced.tsv"
+ANALYTIC_OUTPUT_FILE = "NwssAnalyticEnhanced.tsv"
+RAW_OUTPUT_SAMPLE = "NwssRawEnhancedSample.tsv"
+ANALYTIC_OUTPUT_SAMPLE = "NwssAnalyticEnhancedSample.tsv"
+SAMPLE_SIZE = 1000
 
 
-# Wastewater samples raw data. Acquired by restricted download from CDC, after signing data-use agreement.
+# Wastewater samples raw data and analytics data. Acquired by restricted download from CDC, after signing data-use agreement.
 
 path = "~/Desktop/COVID Programming/CDC/cdc-nwss-restricted-data-set-wastewater-2022-02-08.csv"
-SampleDF = pd.read_csv(path, sep=',', header='infer', dtype=str)
+RawDF = pd.read_csv(path, sep=',', header='infer', dtype=str)
+
+path = "~/Desktop/COVID Programming/CDC/cdc-nwss-restricted-data-set-final-2022-02-08.csv"
+AnalyticDF = pd.read_csv(path, sep=',', header='infer', dtype=str)
 
 # Covid facts per county from https://apidocs.covidactnow.org. You need an API key, which they give to anyone who asks.
 
@@ -33,13 +40,22 @@ PopDF = pd.read_csv(path, sep=',', header='infer', dtype=str, encoding='latin-1'
 CovidDF = CovidDF.rename(columns={"metrics.caseDensity":"metrics.caseDensity100k", "date":"covid_facts_date"})
 CovidDF["covid_facts_date"] = pd.to_datetime(CovidDF["covid_facts_date"], errors='coerce')
 
-# Make a few changes to the wastewater samples file.
+# Make a few changes to the wastewater samples files.
 
-SampleDF["sample_collect_date"] = pd.to_datetime(SampleDF["sample_collect_date"], errors='coerce')
-SampleDF = SampleDF.rename(columns={"county_names": "CountyFIPS"})
-SampleDF["CountyFIPS"] = SampleDF["CountyFIPS"].str.split(",")   
-SampleDF = SampleDF.explode("CountyFIPS")          # One county per row
-SampleDF["CountyFIPS"] = SampleDF["CountyFIPS"].str.strip("[]' ")
+RawDF["sample_collect_date"] = pd.to_datetime(RawDF["sample_collect_date"], errors='coerce')
+
+RawDF = RawDF.rename(columns={"county_names": "CountyFIPS"})
+RawDF["CountyFIPS"] = RawDF["CountyFIPS"].str.split(",")    # change comma separate string to array
+RawDF = RawDF.explode("CountyFIPS")          # make one row per county
+RawDF["CountyFIPS"] = RawDF["CountyFIPS"].str.strip("[]' ")   # clean up, one pure FIPS per row
+
+AnalyticDF = AnalyticDF.rename(columns={"date": "sample_collect_date"})
+AnalyticDF["sample_collect_date"] = pd.to_datetime(AnalyticDF["sample_collect_date"], errors='coerce')
+
+AnalyticDF = AnalyticDF.rename(columns={"county_names": "CountyFIPS"})
+AnalyticDF["CountyFIPS"] = AnalyticDF["CountyFIPS"].str.split(",")   
+AnalyticDF = AnalyticDF.explode("CountyFIPS")
+AnalyticDF["CountyFIPS"] = AnalyticDF["CountyFIPS"].str.strip("[]' ")
 
 # Make a few changes to the population file.
 
@@ -52,60 +68,103 @@ PopDF = PopDF[PopDF.POPESTIMATE2020 > 0]     # throw out bad data
 # Add county population to each sample row. 
 # Numbers get changed to float during merge, so fix back to integer.
 
-SampleDF = SampleDF.merge(PopDF, how='left', on="CountyFIPS")
-SampleDF["POPESTIMATE2020"] = pd.to_numeric(SampleDF["POPESTIMATE2020"], errors='coerce').fillna(0).astype(int)
+RawDF = RawDF.merge(PopDF, how='left', on="CountyFIPS")
+RawDF["POPESTIMATE2020"] = pd.to_numeric(RawDF["POPESTIMATE2020"], errors='coerce').fillna(0).astype(int)
 
-# Create some additional date columns on the sample file. These will be used to add the 
+AnalyticDF = AnalyticDF.merge(PopDF, how='left', on="CountyFIPS")
+AnalyticDF["POPESTIMATE2020"] = pd.to_numeric(AnalyticDF["POPESTIMATE2020"], errors='coerce').fillna(0).astype(int)
+
+# Create some additional date columns on the sample files. These will be used to add the 
 # look ahead / look back info.
 
-SampleDF["vax_date"] = SampleDF["sample_collect_date"] -  pd.offsets.Day(VAX_LOOK_BACK)
-SampleDF["cases_date"] = SampleDF["sample_collect_date"] + pd.offsets.Day(CASES_LOOK_AHEAD)
-SampleDF["hosp_date"] = SampleDF["sample_collect_date"] +  pd.offsets.Day(HOSP_LOOK_AHEAD)
-SampleDF["deaths_date"] = SampleDF["sample_collect_date"] +  pd.offsets.Day(DEATHS_LOOK_AHEAD)
+RawDF["vax_date"] = RawDF["sample_collect_date"] -  pd.offsets.Day(VAX_LOOK_BACK)
+RawDF["cases_date"] = RawDF["sample_collect_date"] + pd.offsets.Day(CASES_LOOK_AHEAD)
+RawDF["hosp_date"] = RawDF["sample_collect_date"] +  pd.offsets.Day(HOSP_LOOK_AHEAD)
+RawDF["deaths_date"] = RawDF["sample_collect_date"] +  pd.offsets.Day(DEATHS_LOOK_AHEAD)
+
+AnalyticDF["vax_date"] = AnalyticDF["sample_collect_date"] -  pd.offsets.Day(VAX_LOOK_BACK)
+AnalyticDF["cases_date"] = AnalyticDF["sample_collect_date"] + pd.offsets.Day(CASES_LOOK_AHEAD)
+AnalyticDF["hosp_date"] = AnalyticDF["sample_collect_date"] +  pd.offsets.Day(HOSP_LOOK_AHEAD)
+AnalyticDF["deaths_date"] = AnalyticDF["sample_collect_date"] +  pd.offsets.Day(DEATHS_LOOK_AHEAD)
 
 # Make a subset of just the vax facts we want and join it to the samples.
 # Note that we join the vax info from a previous date onto each sample.
 
 VaxDF = CovidDF[["covid_facts_date", "fips", "actuals.vaccinationsInitiated", "actuals.vaccinationsCompleted", "actuals.vaccinationsAdditionalDose", "metrics.vaccinationsInitiatedRatio", "metrics.vaccinationsCompletedRatio", "metrics.vaccinationsAdditionalDoseRatio"]]
 
-SampleDF = SampleDF.merge(VaxDF, how='left', left_on=["vax_date", "CountyFIPS"], right_on=["covid_facts_date", "fips"])
-SampleDF = SampleDF.drop(columns=["covid_facts_date", "fips"])
+RawDF = RawDF.merge(VaxDF, how='left', left_on=["vax_date", "CountyFIPS"], right_on=["covid_facts_date", "fips"])
+RawDF = RawDF.drop(columns=["covid_facts_date", "fips"])
+
+AnalyticDF = AnalyticDF.merge(VaxDF, how='left', left_on=["vax_date", "CountyFIPS"], right_on=["covid_facts_date", "fips"])
+AnalyticDF = AnalyticDF.drop(columns=["covid_facts_date", "fips"])
 
 # Make a subset of just the covid case facts we want and join it to the samples.
 # Note that we join the case info from a future date onto each sample.
 
 CasesDF = CovidDF[["covid_facts_date", "fips", "actuals.newCases", "metrics.caseDensity100k", "metrics.infectionRate", "metrics.testPositivityRatio"]]
 
-SampleDF = SampleDF.merge(CasesDF, how='left', left_on=["cases_date", "CountyFIPS"], right_on=["covid_facts_date", "fips"])
-SampleDF = SampleDF.drop(columns=["covid_facts_date", "fips"])
+RawDF = RawDF.merge(CasesDF, how='left', left_on=["cases_date", "CountyFIPS"], right_on=["covid_facts_date", "fips"])
+RawDF = RawDF.drop(columns=["covid_facts_date", "fips"])
+
+AnalyticDF = AnalyticDF.merge(CasesDF, how='left', left_on=["cases_date", "CountyFIPS"], right_on=["covid_facts_date", "fips"])
+AnalyticDF = AnalyticDF.drop(columns=["covid_facts_date", "fips"])
 
 # Add hospitialization facts to the sample file.
 
 HospDF = CovidDF[["covid_facts_date", "fips", "actuals.icuBeds.currentUsageCovid", "actuals.hospitalBeds.currentUsageCovid"]]
 
-SampleDF = SampleDF.merge(HospDF, how='left', left_on=["hosp_date", "CountyFIPS"], right_on=["covid_facts_date", "fips"])
-SampleDF = SampleDF.drop(columns=["covid_facts_date", "fips"])
+RawDF = RawDF.merge(HospDF, how='left', left_on=["hosp_date", "CountyFIPS"], right_on=["covid_facts_date", "fips"])
+RawDF = RawDF.drop(columns=["covid_facts_date", "fips"])
+
+AnalyticDF = AnalyticDF.merge(HospDF, how='left', left_on=["hosp_date", "CountyFIPS"], right_on=["covid_facts_date", "fips"])
+AnalyticDF = AnalyticDF.drop(columns=["covid_facts_date", "fips"])
 
 # Add mortality info
 
 DeathsDF = CovidDF[["covid_facts_date", "fips", "actuals.newDeaths"]]
 
-SampleDF = SampleDF.merge(DeathsDF, how='left', left_on=["deaths_date", "CountyFIPS"], right_on=["covid_facts_date", "fips"])
-SampleDF = SampleDF.drop(columns=["covid_facts_date", "fips"])
+RawDF = RawDF.merge(DeathsDF, how='left', left_on=["deaths_date", "CountyFIPS"], right_on=["covid_facts_date", "fips"])
+RawDF = RawDF.drop(columns=["covid_facts_date", "fips"])
 
-# Debugging
+AnalyticDF = AnalyticDF.merge(DeathsDF, how='left', left_on=["deaths_date", "CountyFIPS"], right_on=["covid_facts_date", "fips"])
+AnalyticDF = AnalyticDF.drop(columns=["covid_facts_date", "fips"])
 
-#print (SampleDF.head(10), "\n")
-#print (CovidDF.head(10), "\n")
-#print (PopDF.head(10), "\n")
+# Write to files.
 
-#print (SampleDF.dtypes, "\n")
-#print (CovidDF.dtypes, "\n")
-#print (PopDF.dtypes, "\n")
+print ("\nWriting enhanced raw wastewater data to", RAW_OUTPUT_FILE, "with", RawDF.shape[0], "rows.\n")
+RawDF.to_csv(RAW_OUTPUT_FILE, encoding='utf-8', sep='\t', index=False)
 
-     
-# Write to file.
+print ("Writing enhanced analytic wastewater data to", ANALYTIC_OUTPUT_FILE, "with", AnalyticDF.shape[0], "rows.\n")
+AnalyticDF.to_csv(ANALYTIC_OUTPUT_FILE, encoding='utf-8', sep='\t', index=False)
 
-print ("Writing enhanced wastewater samples to", SAMPLES_OUTPUT_FILE, "with", SampleDF.shape[0], "rows.\n")
-SampleDF.to_csv(SAMPLES_OUTPUT_FILE, encoding='utf-8', sep='\t', index=False)
+# Create and write anonymized sample files.
+
+RawSampleDF = RawDF.sample(n=SAMPLE_SIZE)
+AnalyticSampleDF = AnalyticDF.sample(n=SAMPLE_SIZE)
+
+RawSampleDF["CountyFIPS"] = "XXXX"
+RawSampleDF["sample_location"] = "XXXX"
+RawSampleDF["sample_location_specify"] = "XXXX"
+RawSampleDF["wwtp_name"] = "XXXX"
+RawSampleDF["lab_id"] = "XXXX"
+RawSampleDF["key_sewershed"] = "XXXX"
+
+AnalyticSampleDF["CountyFIPS"] = "XXXX"
+AnalyticSampleDF["sample_location"] = "XXXX"
+AnalyticSampleDF["sample_location_specify"] = "XXXX"
+AnalyticSampleDF["wwtp_name"] = "XXXX"
+AnalyticSampleDF["lab_id"] = "XXXX"
+AnalyticSampleDF["key_plot"] = "XXXX"
+
+print ("\nWriting anonymized sample of enhanced raw wastewater data to", RAW_OUTPUT_SAMPLE, "with", RawSampleDF.shape[0], "rows.\n")
+RawSampleDF.to_csv(RAW_OUTPUT_SAMPLE, encoding='utf-8', sep='\t', index=False)
+
+print ("Writing anonymized sample of enhanced analytic wastewater data to", ANALYTIC_OUTPUT_SAMPLE, "with", AnalyticSampleDF.shape[0], "rows.\n")
+AnalyticSampleDF.to_csv(ANALYTIC_OUTPUT_SAMPLE, encoding='utf-8', sep='\t', index=False)
+
+
+
+
+
+
 
