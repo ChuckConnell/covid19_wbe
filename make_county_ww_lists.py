ae65a6eac327b4bf1764efe8b05ff1cf72c2945c
Chuck Connell <chuck.connell.3@gmail.com>
2022-04-04 16:53:43 -0400

    Updated data, added pop density

--- make_county_ww_lists.py
+++ make_county_ww_lists.py
@@ -5,13 +5,13 @@
 import pandas as pd 
 from urllib import request
 
-USA_COUNTIES_DOWNLOAD = "https://github.com/ChuckConnell/articles/raw/master/fips2county.tsv"
 BIOBOT_DOWNLOAD = "https://github.com/biobotanalytics/covid19-wastewater-data/raw/master/wastewater_by_county.csv"
 NWSS_DOWNLOAD = "https://data.cdc.gov/resource/2ew6-ywp6.csv"  # this is suposed to be the API url, but it does not get all 156k rows, I think it is a CDC bug
 
 USA_COUNTIES_LOCAL = "/Users/chuck/Desktop/COVID Programming/fips2county.tsv"
+COUNTY_POP_LOCAL = "/Users/chuck/Desktop/COVID Programming/US Census/Population_Density_County.csv"
 BIOBOT_LOCAL = "/Users/chuck/Desktop/COVID Programming/Biobot/wastewater_by_county.csv"
-NWSS_LOCAL = "/Users/chuck/Desktop/COVID Programming/CDC/NWSS_Public_SARS-CoV-2_Wastewater_Data.tsv"  # i got this manually for now
+NWSS_LOCAL = "/Users/chuck/Desktop/COVID Programming/CDC/NWSS_Public_SARS-CoV-2_Wastewater_Metric_Data.tsv"  # i got this manually for now
 
 BIOBOT_LIST = "biobot_counties.txt"
 NWSS_LIST = "nwss_counties.txt"
@@ -23,13 +23,19 @@ MISSING_COUNTIES_LIST = "missing_counties.txt"
 FIPS_MAP_DATA = "wbe_coverage_map.tsv"
 
 # Get the list of all USA counties and tweak as we need it. 
+# Downloaded once from https://github.com/ChuckConnell/articles/raw/master/fips2county.tsv
 
-request.urlretrieve(USA_COUNTIES_DOWNLOAD, USA_COUNTIES_LOCAL)
 AllCountiesDF = pd.read_csv(USA_COUNTIES_LOCAL, sep='\t', header='infer', dtype=str)
-
 AllCountiesDF["STATE_COUNTY_FIPS"] = AllCountiesDF["STATE_COUNTY"] + " | " + AllCountiesDF["CountyFIPS"]  # Add a useful field that combines state+county+fips. 
-
 AllCountiesDF = AllCountiesDF[["CountyFIPS", "STATE_COUNTY_FIPS"]]  # don't need any other columns
+AllCountiesDF = AllCountiesDF.rename(columns={"CountyFIPS":"FIPS"})  # to match Flourish naming
+
+# Get the population density of US counties, and tweak as needed.
+# This is June 2020 data that I downloaded once from https://covid19.census.gov/datasets/21843f238cbb46b08615fc53e19e0daf/explore
+
+CountyPopDF = pd.read_csv(COUNTY_POP_LOCAL, sep=',', header='infer', dtype=str)
+CountyPopDF = CountyPopDF[["GEOID", "B01001_calc_PopDensity"]]
+CountyPopDF = CountyPopDF.rename(columns={"GEOID":"FIPS", "B01001_calc_PopDensity":"DensitySqKm"})  
 
 # Get the latest counties covered by Biobot, and tweak as we need it.
 
@@ -37,8 +43,9 @@ request.urlretrieve(BIOBOT_DOWNLOAD, BIOBOT_LOCAL)
 BiobotDF = pd.read_csv(BIOBOT_LOCAL, sep=',', header='infer', dtype=str)
 
 BiobotDF = BiobotDF[["fipscode"]]  # don't need any other columns
-BiobotDF.loc[BiobotDF["fipscode"].str.len() == 4, "fipscode"] = "0" + BiobotDF["fipscode"]  # fix problem with missing leading zeroes 
-BiobotDF = BiobotDF.merge(AllCountiesDF, how="left", left_on=["fipscode"], right_on=["CountyFIPS"])  # add readable names
+BiobotDF = BiobotDF.rename(columns={"fipscode":"FIPS"})  # to match Flourish naming
+BiobotDF.loc[BiobotDF["FIPS"].str.len() == 4, "FIPS"] = "0" + BiobotDF["FIPS"]  # fix problem with missing leading zeroes 
+BiobotDF = BiobotDF.merge(AllCountiesDF, how="left", on=["FIPS"])  # add readable names
 
 # Get the latest counties covered by NWSS. We grab this from their public dataset, not the special restricted data.
 # This dataset sometimes has more than one FIPS per row, so we have to "normalize" and explode these rows.
@@ -47,12 +54,13 @@ BiobotDF = BiobotDF.merge(AllCountiesDF, how="left", left_on=["fipscode"], right
 NwssDF = pd.read_csv(NWSS_LOCAL, sep='\t', header='infer',  dtype=str)
 
 NwssDF = NwssDF[["county_fips"]]  # don't need any other columns
+NwssDF = NwssDF.rename(columns={"county_fips":"FIPS"})  # to match Flourish naming
 
-NwssDF["county_fips"] = NwssDF["county_fips"].str.split(",")    # change comma separate string to array
-NwssDF = NwssDF.explode("county_fips")          # make one row per county
-NwssDF["county_fips"] = NwssDF["county_fips"].str.strip("[]' ")   # clean up, one pure FIPS per row
+NwssDF["FIPS"] = NwssDF["FIPS"].str.split(",")    # change comma separate string to array
+NwssDF = NwssDF.explode("FIPS")          # make one row per county
+NwssDF["FIPS"] = NwssDF["FIPS"].str.strip("[]' ")   # clean up, one pure FIPS per row
 
-NwssDF = NwssDF.merge(AllCountiesDF, how="left", left_on=["county_fips"], right_on=["CountyFIPS"])  # add readable names
+NwssDF = NwssDF.merge(AllCountiesDF, how="left", on=["FIPS"])  # add readable names
 
 # For each data source, get only the field we want in the output lists, and put them into a Python set.
 
@@ -105,7 +113,16 @@ MapDF["WBE_WHO"] = "none"
 MapDF.loc[MapDF["STATE_COUNTY_FIPS"].isin(biobot_only), "WBE_WHO"] = "biobot"
 MapDF.loc[MapDF["STATE_COUNTY_FIPS"].isin(nwss_only), "WBE_WHO"] = "nwss"
 MapDF.loc[MapDF["STATE_COUNTY_FIPS"].isin(biobot_nwss_intersection), "WBE_WHO"] = "biobot-nwss"
-MapDF = MapDF.rename(columns={"CountyFIPS":"FIPS"})  # to match Flourish naming
     
+# Add population density in the counties, but only for counties where we have data.
+
+MapDF = MapDF.merge(CountyPopDF, how="left", on=["FIPS"])  # add pop density for all
+MapDF.loc[MapDF["STATE_COUNTY_FIPS"].isin(missing_counties), "DensitySqKm"] = ""   # suppress counties with no WBE data 
+MapDF["DensitySqKm"] = pd.to_numeric(MapDF["DensitySqKm"], errors='coerce').round(1)
+MapDF["DensityCategory"] = pd.qcut(MapDF['DensitySqKm'], 3, labels=["low", "med", "high"])
+
+# Write out the map data fil.
+
 print ("\nWriting map data to " + FIPS_MAP_DATA)
 MapDF.to_csv(FIPS_MAP_DATA, encoding='utf-8', sep='\t', index=False)
+
